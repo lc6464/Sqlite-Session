d0051de26f3ba6b8ddba336372394ec387ffe847
NULL-LC <64722907+NULL-LC@users.noreply.github.com>
2020-11-29 15:00:39 +0800

    完成 setup.py，完善介绍。

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup, find_packages
 
-setup(
+with open("README.md", encoding="utf-8") as f:
+	long_description = f.read()
 
+
+setup(
+	name="Sqlite-Session",
+	description="Use SQLite to store session data on the server.",
+	version="0.1.0",
+	packages=find_packages(),
+	url="https://github.com/lc6464/Sqlite-Session",
+	license="MIT License",
+	long_description=long_description,
+	long_description_content_type="text/markdown"
 )
